bdfcf702283d829d20134aa1cec15a9a03241fea
Daniel Ramírez Barrientos <barrientosd9@gmail.com>
2023-07-09 15:27:45 -0600

    Remove the Refactor scaffolding schematic

--- src/ng-generate/scaffolding/index.ts
+++ src/ng-generate/scaffolding/index.ts
@@ -48,7 +48,7 @@ export function scaffolding(options: ScaffoldOptions): Rule {
     );
     return chain([
       scaffoldFoldersFactory(structures, options),
-      deleteFile(options),
+      options.deleteFile ? deleteFile(options) : noop(),
       printFinalMessage(),
     ]);
   };
