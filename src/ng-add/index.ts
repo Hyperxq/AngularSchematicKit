ecbe903bc8cd919b0571b8bab88b04967df9b994
Daniel Ramírez Barrientos <barrientosd9@gmail.com>
2023-07-11 18:28:11 -0600

    Schema added to collection

--- src/ng-add/index.ts
+++ src/ng-add/index.ts
@@ -3,6 +3,7 @@ import { chain, Rule, SchematicContext, Tree } from '@angular-devkit/schematics'
 export default function (): Rule {
   return (_host: Tree, context: SchematicContext) => {
     context.logger.log('info', `You can use these schematics to your project, only copy the name`);
+    context.logger.log('info', `✨- initialize-angular-configuration (alias: start)`);
     context.logger.log('info', `✨- add-linters (alias: l)`);
     context.logger.log('info', `✨- add-git-hooks  (alias: gh)`);
     context.logger.log('info', `✨- scaffolding  (alias: s)`);
