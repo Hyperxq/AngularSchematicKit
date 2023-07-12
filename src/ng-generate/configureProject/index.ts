c91cfa88fc3030a60d63cfbc1a76cd2e27d9150e
Daniel Ramírez Barrientos <barrientosd9@gmail.com>
2023-07-11 18:40:02 -0600

    Schema bug fixed

--- src/ng-generate/configureProject/index.ts
+++ src/ng-generate/configureProject/index.ts
@@ -1,11 +1,13 @@
 import { ConfigureProjectOptions } from './configure-project.interfaces';
-import { Rule, schematic } from '@angular-devkit/schematics';
+import { chain, Rule, schematic } from '@angular-devkit/schematics';
 
 export function configureProject(options: ConfigureProjectOptions): Rule {
   return () => {
     const { schematicNames } = options;
+    let calls: Rule[] = [];
     schematicNames.forEach((schematicName) => {
-      schematic(schematicName, {});
+      calls.push(schematic(schematicName, {}));
     });
+    return chain(calls);
   };
 }
